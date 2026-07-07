ff90d36dd539fd0ce45b8b7de3fae1cb8d92c818
hwollwage <hanzelputrawollwage@gmail.com>
2026-07-08 02:38:04 +0700

    ya ya saya setuju

---
+++ python/color_detect.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("camera not found")
+        break
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_red1 = np.array([0,120,70])
+    upper_red1 = np.array([10,255,255])
+
+    lower_red2 = np.array([170,120,70])
+    upper_red2 = np.array([180,255,255])
+    
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = mask1 + mask2
+    
+
+    cv2.imshow("camera", frame)
+    cv2.imshow("hsv", mask)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ python/servo_color.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy
+import serial
+
+serial.Serial("/dev/ttyUSB0", 115200)
+
